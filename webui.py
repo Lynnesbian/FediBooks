0703c4df2cbe584b8c88f3d687129abd1f4cc8ad
Lynne <lynne@bune.city>
2019-08-28 13:53:44 +1000

    removed bootstrap dependency and made my own css

--- webui.py
+++ webui.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
 	return render_template("front_page.html")
+
+@app.route("/login")
+def show_login_page():
+	return render_template("login.html")
