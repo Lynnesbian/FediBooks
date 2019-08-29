c7e8aeabdb4a2f37e32e19ffa031c11312548f29
Lynne <lynne@bune.city>
2019-08-29 11:15:47 +1000

    show welcome page before login page

--- webui.py
+++ webui.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello():
 	return render_template("front_page.html")
 
+@app.route("/welcome")
+def welcome():
+	return render_template("welcome.html")
+
 @app.route("/login")
 def show_login_page():
 	return render_template("login.html")
